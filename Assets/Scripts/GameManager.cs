2eed806e6c306ac0d6908ee0b7216ca5235ffd95
Benjamin Kovach <bkovach13@gmail.com>
2019-07-05 09:07:20 -0400

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float turnDelay = 0.1f;
+    public static GameManager instance = null;
+
+    public BoardManager boardScript;
+
+    private int level = 3;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+
+    public GameObject playerPrefab;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        enemies = new List<Enemy>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        Instantiate(playerPrefab, new Vector3(0, 0), Quaternion.identity);
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    void OnLevelWasLoaded(int _level)
+    {
+        // TODO: Refactor
+        level++;
+        InitGame();
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        foreach(Enemy enemy in enemies)
+        {
+            enemy.MoveEnemy();
+        }
+        yield return new WaitForSeconds(turnDelay);
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+
+    public void GameOver()
+    {
+        print("game over");
+        enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playersTurn || enemiesMoving)
+            return;
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 0.5f;
+
+    private Animator animator;
+    private int food;
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+
+        animator.SetTrigger("playerChop");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        } else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            other.gameObject.SetActive(false);
+        } else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
+    private void Restart()
+    {
+        // NB. Only one scene, because procedural generation
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        CheckIfGameOver();
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        // TODO: Fail move attempt if other entity exists in previous
+        // phase in turn
+        base.AttemptMove<T>(xDir, yDir);
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = (int)Input.GetAxisRaw("Horizontal");
+        int vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+            GameManager.instance.GameOver();
+    }
+}

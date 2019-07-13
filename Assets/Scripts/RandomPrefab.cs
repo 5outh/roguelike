9f60b963248cb7d50f6b57ee9e33f3ab3631a2b0
Benjamin Kovach <bkovach13@gmail.com>
2019-07-13 10:16:30 -0400

    Add RandomPrefab

---
+++ Assets/Scripts/RandomPrefab.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// Replace this GameObject with a random prefab from a specified list.
+public class RandomPrefab : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    GameObject GetRandomPrefab()
+    {
+        return prefabs[Random.Range(0, prefabs.Length)];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(GetRandomPrefab(), transform.position, Quaternion.identity, transform.parent);
+        Destroy(gameObject);
+    }
+}
